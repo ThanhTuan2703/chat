7ec659d085a89ca4a1aca96872387c5d2e2069f9
ThanhTuan2703 <thanhtuan27032012@gmail.com>
2022-04-19 16:23:04 +0700

    update code

---
+++ SQLQuery1.sql
@@ -0,0 +1,31 @@
+create table Person(
+	Id int IDENTITY(1,1) PRIMARY KEY,
+    Name varchar(255),
+	Description varchar(255)
+)
+
+create table Message(
+	Id int IDENTITY(1,1) PRIMARY KEY,
+	DirectId int FOREIGN KEY REFERENCES Person(Id),
+	ReceiverId int FOREIGN KEY REFERENCES Person(Id),
+	MessageText nvarchar(255)
+)
+
+drop table Message;
+drop table Person;
+
+insert into Person(Name)
+values ('Server'),
+('Client')
+
+select * from Person;
+select * from Message;
+
+UPDATE Person SET Name = 'Name2' WHERE Id = 2 
+
+INSERT INTO Message(DirectId,ReceiverId,MessageText)
+VALUES (1, 2, 'MESS');
+
+SELECT message.id, sendPerson.name as 'Sender Name', receiverPerson.name as 'Receiver Name', message.MessageText as 'Message Text'  FROM Message message 
+JOIN Person receiverPerson ON message.ReceiverId = receiverPerson.id
+JOIN Person sendPerson ON message.DirectId = sendPerson.id;
---
+++ src/database/DatabaseManager.java
@@ -0,0 +1,57 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+	private static final String Q_UPDATE_CLIENT = "UPDATE Person SET Name = ? WHERE Id = 2 ";
+	private static final String Q_SAVE_MESSAGE = "INSERT INTO Message(DirectId,ReceiverId,MessageText) VALUES (?, ?, ?)";
+
+	public static Connection openConnection() throws Exception {
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		String connectionUrl = "jdbc:sqlserver://DESKTOP-QHMC6QJ:1433;" + "databaseName=ChatApp;User=sa;Password=sa";
+		Connection con = DriverManager.getConnection(connectionUrl);
+		System.out.println("connect successfully");
+		return con;
+	}
+
+	public static void updateClientName(String name) {
+		Connection connection;
+		try {
+			connection = openConnection();
+			PreparedStatement pst = connection.prepareStatement(Q_UPDATE_CLIENT,
+					PreparedStatement.RETURN_GENERATED_KEYS);
+			pst.setString(1, name);
+			pst.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void saveMessage(String sender, String message) {
+		Connection connection;
+		try {
+			connection = openConnection();
+			PreparedStatement pst = connection.prepareStatement(Q_SAVE_MESSAGE,
+					PreparedStatement.RETURN_GENERATED_KEYS);
+			if (sender.equals("Manager")) {
+				pst.setInt(1, 1);
+				pst.setInt(2, 2);
+			} else {
+				pst.setInt(1, 2);
+				pst.setInt(2, 1);
+			}
+			pst.setString(3, message);
+			pst.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
